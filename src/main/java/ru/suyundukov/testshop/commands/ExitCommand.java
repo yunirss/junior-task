29636b00628d6cd1e66c18ae38f74fc833df33fc
yunirss <yunir14@yandex.ru>
2023-05-18 18:42:57 +0300

    тест 2

--- src/main/java/ru/suyundukov/testshop/commands/ExitCommand.java
+++ src/main/java/ru/suyundukov/testshop/commands/ExitCommand.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ExitCommand implements Command{
-    //выход
     @Override
     public void execute() {
         System.exit(0);
